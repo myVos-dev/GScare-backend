c180a977c96c92b7e71c03656c9c72cebf220900
Popstar <joel.lake.777@gmail.com>
2024-05-11 23:37:35 -0300

    adicionando arquivos de testes

---
+++ tests/CommonTestUtils/Requests/RequestRegisterEmployeeJsonBuilder.cs
@@ -0,0 +1,43 @@
+﻿using Bogus;
+using GscareApiAspNetCore.Communication.Enums;
+using GscareApiAspNetCore.Communication.Requests;
+namespace CommonTestUtils.Requests;
+public class RequestRegisterEmployeeJsonBuilder
+{
+    public static RequestRegisterEmployeeJson Build()
+    {
+        //var faker = new Faker();
+
+        //var request = new RequestRegisterEmployeeJson
+        //{
+        //    NomeCompleto = faker.Name.FullName(),
+        //};
+
+        return new Faker<RequestRegisterEmployeeJson>()
+            .RuleFor(r => r.NomeCompleto, faker => faker.Name.FullName())
+            .RuleFor(r => r.DisponibilidadeDeHorario, faker => faker.PickRandom<DisponibilidadeDeHorarioType>())
+            .RuleFor(r => r.CertificadoDoCurso, faker => faker.Address.CountryCode())
+            .RuleFor(r => r.Cpf, faker => faker.Company.CompanyName())
+            .RuleFor(r => r.EnderecoComCep, faker => faker.Company.CompanyName())
+            .RuleFor(r => r.FotoComprovanteResidencia, faker => faker.Company.CompanyName())
+            .RuleFor(r => r.FotoCpf, faker => faker.Company.CompanyName())
+            .RuleFor(r => r.FotoIdentidade, faker => faker.Company.CompanyName())
+            .RuleFor(r => r.Identidade, faker => faker.Company.CompanyName())
+            .RuleFor(r => r.Mei, faker => faker.Company.CompanyName())
+            .RuleFor(r => r.NadaConsta, faker => faker.Company.CompanyName());
+
+        //{
+        //    NomeCompleto = "fernando franco",
+        //    DisponibilidadeDeHorario = 0,
+        //    CertificadoDoCurso = "",
+        //    Cpf = "",
+        //    EnderecoComCep = "",
+        //    FotoComprovanteResidencia = "",
+        //    FotoCpf = "",
+        //    FotoIdentidade = "",
+        //    Identidade = "",
+        //    Mei = "",
+        //    NadaConsta = ""
+        //};
+    }
+}
---
+++ tests/Validators.Test/Employee/RegisterEmployeeValidatorTests.cs
@@ -0,0 +1,58 @@
+﻿using CommonTestUtils.Requests;
+using FluentAssertions;
+using GscareApiAspNetCore.Application.UseCases;
+using GscareApiAspNetCore.Communication.Enums;
+using GscareApiAspNetCore.Exception;
+
+namespace Validators.Test.Employee;
+public class RegisterEmployeeValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new RegisterEmployeeValidator();
+        var request = RequestRegisterEmployeeJsonBuilder.Build();
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("    ")]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Error_NomeCompleto_Empty(string fullName)
+    {
+        //Arrange
+        var validator = new RegisterEmployeeValidator();
+        var request = RequestRegisterEmployeeJsonBuilder.Build();
+        request.NomeCompleto = fullName;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.FULL_NAME_IS_REQUIRED));
+    }
+
+    [Fact]
+    public void Error_DisponibilidadeDeHorario_Invalid()
+    {
+        //Arrange
+        var validator = new RegisterEmployeeValidator();
+        var request = RequestRegisterEmployeeJsonBuilder.Build();
+        request.DisponibilidadeDeHorario = (DisponibilidadeDeHorarioType)800;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TIME_AVAILABILITY_TYPE_IS_NOT_VALID_));
+    }
+}
